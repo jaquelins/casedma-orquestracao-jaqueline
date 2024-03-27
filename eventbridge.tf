d022a9730d1c7092ec4d3b7cd879b6da260f8b4b
Jaqueline Lins Araujo <jlinsara@emeal.nttdata.com>
2024-03-27 10:59:01 -0300

    Criando um event bridge e step functions t4

--- eventbridge.tf
+++ eventbridge.tf
@@ -1,14 +1,11 @@
-# Recurso para a regra de evento do Eventbridge #
 resource "aws_cloudwatch_event_rule" "produto_credito_eventbridge_jaqueline" {
   name                = "produto_credito_eventbridge_jaqueline"
   description = "Agendamento para execução mensal do produto de crédito"
-  schedule_expression = "cron(0 21 06 * ? *)"  # Executar todo dia 06 de todos os meses às 18 horas
+  schedule_expression = "cron(0 21 06 * ? *)"
   tags = {
     DMA = "DataMeshAcademy"
   }
 }
- 
-# Amarrando a step function a regra #
 resource "aws_cloudwatch_event_target" "target_produto_credito_jaqueline" {
     rule = aws_cloudwatch_event_rule.produto_credito_eventbridge_jaqueline.name
     arn = aws_sfn_state_machine.produto_credito_state_machine_jaqueline.arn
