838606e42daec7477f19a5a5a45c66291f6a000a
Jaqueline Lins Araujo <jlinsara@emeal.nttdata.com>
2024-03-27 11:24:42 -0300

    Criando um event bridge e step functions t10

--- stepfunc.tf
+++ stepfunc.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_log_group" "log_state_machine_produto_credito_jaqueline" {
+resource "aws_cloudwatch_log_group" "log_state_machine_produto_credito" {
   name              = "log_state_machine_produto_credito_jaqueline"
   retention_in_days = 14
   tags = {
@@ -13,10 +13,9 @@ resource "aws_sfn_state_machine" "produto_credito_state_machine_jaqueline" {
   tags = {
     DMA = "DataMeshAcademy"
   }
- 
   logging_configuration {
     level    = "ALL"
     include_execution_data = true
-    log_destination = "${aws_cloudwatch_log_group.log_state_machine_produto_credito_jaqueline.arn}:*"
+    log_destination = "${aws_cloudwatch_log_group.log_state_machine_produto_credito.arn}:*"
   }
 }
